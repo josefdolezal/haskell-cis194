831a4230ae009ba5cdac0e55ef2e614ccbece18a
Josef Dolezal <pepik.ml@gmail.com>
2017-09-27 21:47:38 +0200

    Add solution for exercise 2, week 3

--- week-3/Golf.hs
+++ week-3/Golf.hs
@@ -5,4 +5,11 @@ everyNth n = map snd . filter (\(x,y) -> x `mod` n == 0) . zip [1..]
 
 skips :: [a] -> [[a]]
 skips = map (\(n,xs) -> everyNth n xs) . zip [1..] . clone
-    where clone xs = replicate (length xs) xs 
+    where clone xs = replicate (length xs) xs
+
+triplets :: [a] -> [(a,a,a)]
+triplets (x:y:z:zs) = (x,y,z) : triplets (y : z : zs)
+triplets _ = []
+
+localMaxima :: [Integer] -> [Integer]
+localMaxima = map (\(_,y,_) -> y) . filter (\(x,y,z) -> x < y && y > z) . triplets
